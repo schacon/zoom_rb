bf2651e7c958c2d585bb5f03265b0723de7415da
Scott Chacon <schacon@gmail.com>
2019-07-22 17:29:36 +0200

    make the recording api actually work

--- lib/zoom/actions/recording.rb
+++ lib/zoom/actions/recording.rb
@@ -5,21 +5,15 @@ module Zoom
     module Recording
       def recording_list(*args)
         options = Utils.extract_options!(args)
-        Utils.require_params([:host_id], options)
+        Utils.require_params([:user_id], options)
         Utils.process_datetime_params!(%i[from to], options)
-        Utils.parse_response self.class.get("/meetings/#{options[:host_id]}/recordings",  headers: request_headers)
+        Utils.parse_response self.class.get("/users/#{options[:user_id]}/recordings",  query: options, headers: request_headers)
       end
 
       def recording_get(*args)
         options = Utils.extract_options!(args)
         Utils.require_params([:meeting_id], options)
-        Utils.parse_response self.class.post('/recording/get', query: options)
-      end
-
-      def recording_delete(*args)
-        options = Utils.extract_options!(args)
-        Utils.require_params([:meeting_id], options)
-        Utils.parse_response self.class.post('/recording/delete', query: options)
+        Utils.parse_response self.class.get("/meetings/#{options[:meeting_id]}/recordings", query: options, headers: request_headers)
       end
     end
   end
